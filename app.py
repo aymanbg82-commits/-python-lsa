9c9f00b8e7980f7cb3aa09a86b6983e7477c39f7
aymanbg82-commits <aymanbg82@gmail.com>
2026-06-24 18:10:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+# name = "ayman baig"
+# name = "hamza"
+# phone_number = "hamza"
+# num = 2
+# num2 = 3
+
+# print (num + num2)
+# print("this is my no 85845")
+
+a = "abc10"
+b = 2
+c = 3
+d = 4
+e = "abc12"
+f = "abc13"
+h = a +" "+ e +" "+ f
+i = b + c + d
+j =a +" " +str( b) +" "+ f
+print(h)
+print(i)
+print(j)
+
+
