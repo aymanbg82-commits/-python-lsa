5c33e9ff9e44278bf2bdd679be7f77c4158c8f14
aymanbg82-commits <aymanbg82@gmail.com>
2026-06-30 18:43:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,69 @@
-# name = "ayman baig"
-# name = "hamza"
-# phone_number = "hamza"
-# num = 2
-# num2 = 3
+# # name = "ayman baig"
+# # name = "hamza"
+# # phone_number = "hamza"
+# # num = 2
+# # num2 = 3
 
-# print (num + num2)
-# print("this is my no 85845")
+# # print (num + num2)
+# # print("this is my no 85845")
 
-a = "abc10"
-b = 2
-c = 3
-d = 4
-e = "abc12"
-f = "abc13"
-h = a +" "+ e +" "+ f
-i = b + c + d
-j =a +" " +str( b) +" "+ f
-print(h)
-print(i)
-print(j)
+# a = "abc10"
+# b = 2
+# c = 3
+# d = 4
+# e = "abc12"
+# f = "abc13"
+# h = a +" "+ e +" "+ f
+# i = b + c + d
+# j =a +" " +str( b) +" "+ f
+# print(h)
+# print(i)
+# print(j)
+# Input two numbers from the user
+# # Input length and breadth from the user
+# length = float(input("Enter length: "))
+# breadth = float(input("Enter breadth: "))
 
+# # Calculate and display the area
+# area = length * breadth
+# print("Area of the rectangle:", area)
+# Take name and marks as input
+# 
+# Input the radius from the user
+# 
+# Take name and age as input
+#list - mutable
+# fruits = ["apple","grapes","pineapple","carrot","raddish","potato"]
+# vegitables = ["carrot","raddish","potato"]
+# print(fruits )
+# print (fruits[0:])
+#slicing
+# print (fruits[0:5])
+# print (fruits[:3])
+# print (fruits[::3]) #step slicing
+# #q
+# phone = ["ultra","pro","redmi","vivo ","OnePlus","Apple ","iPhone","Google Pixel","Motorola ","iQOO","samsung"]
+# car = ["Toyota ","Honda Civic","musda","ford","jeep","land c","ferrari","lambo","bugati"]
+# print(phone)
+# print (car)
+# print(phone[-1])
+# print (phone[3:13])
 
+# # add values
+# phone.append("yoo")
+# print(phone)
+
+#insert
+# a = ["car","bike","truck"]
+# a.insert (2,"auto")
+# print(a)
+# #values change
+# a = [1,2,3,4,5]
+# a[1] = 20
+# print(a)
+#q
+phone = ["ultra","pro","redmi","vivo ","OnePlus","Apple ","iPhone","Google Pixel","Motorola ","iQOO","Toyota ","Honda Civic","musda","ford","jeep","land c","ferrari","lambo","bugati","samsung"]
+phone.append("s24 ultra")
+phone.insert(5,"jeep32")
+phone[-2] = "nokia black"
+print(phone)
